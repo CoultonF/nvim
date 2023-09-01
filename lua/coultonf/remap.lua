280dff514de2cc2755499f45010bcb10faf4f263
CoultonF <cjrfraser@gmail.com>
2023-09-01 16:23:24 -0600

    added shortcuts for yanks and buf navigation

--- lua/coultonf/remap.lua
+++ lua/coultonf/remap.lua
@@ -1,4 +1,11 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>ee", vim.cmd.Ex)
 vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("n", "<leader>p", "\"+p")
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.api.nvim_set_keymap("n", "tk", ":bnext<enter>", {noremap=false})
+vim.api.nvim_set_keymap("n", "tj", ":bprev<enter>", {noremap=false})
+vim.api.nvim_set_keymap("n", "th", ":bfirst<enter>", {noremap=false})
+vim.api.nvim_set_keymap("n", "tl", ":blast<enter>", {noremap=false})
+vim.api.nvim_set_keymap("n", "td", ":bdelete<enter>", {noremap=false})
 
